e6342a0c644ca4799378c37633256a9b0b7a15f4
Brett Sutton <bsutton@noojee.com.au>
2020-06-26 23:16:59 +1000

    added main as android studio requires it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:sounds_common/sounds_common.dart';
+
 /// This file exists to make AndroidStudio happy.
 /// With out this file builds will fail.
 
-void main() {}
+void main() {
+  Log.e('This should never be called as its a dummy');
+}
