a443870081eeb9aa48d87cf196d745cb1eb4fec0
Brett Sutton <bsutton@noojee.com.au>
2020-06-29 21:49:30 +1000

    added log10 test

---
+++ test/log10_test.dart
@@ -0,0 +1,9 @@
+import 'package:dart_numerics/dart_numerics.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('log10', () {
+    print(log10(10));
+    print(log10(-110));
+  });
+}
