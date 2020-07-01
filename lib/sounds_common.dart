430fdcf0f4c9c4f052d0bc836030728ccdcd61db
Brett Sutton <bsutton@noojee.com.au>
2020-07-01 10:04:42 +1000

    exposed the Downloader class.

--- lib/sounds_common.dart
+++ lib/sounds_common.dart
@@ -8,6 +8,7 @@ export 'src/track.dart';
 export 'src/util/log.dart';
 export 'src/util/ansi_color.dart';
 export 'src/playback_disposition.dart';
+export 'src/util/downloader.dart';
 export 'src/util/format.dart';
 export 'src/media_format/media_format.dart';
 export 'src/media_format/media_format_manager.dart';
