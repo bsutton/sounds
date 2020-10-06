f4918831e7f72e240d348ef38f8cb63c9426ca40
Brett Sutton <bsutton@noojee.com.au>
2020-10-07 10:18:10 +1100

    New files created by flutter create for ios.

---
+++ ios/Classes/SoundsPlugin.m
@@ -0,0 +1,20 @@
+#import "SoundsPlugin.h"
+
+@implementation SoundsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"sounds"
+            binaryMessenger:[registrar messenger]];
+  SoundsPlugin* instance = [[SoundsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
