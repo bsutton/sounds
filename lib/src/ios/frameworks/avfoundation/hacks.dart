63cc089bc04d9042ff9027595f2139b18c01b30d
Brett Sutton <bsutton@noojee.com.au>
2020-11-10 09:06:40 +1100

    added draft ffi layer for ios.

---
+++ lib/src/ios/frameworks/avfoundation/hacks.dart
@@ -0,0 +1,47 @@
+import 'dart:ffi';
+
+import 'package:dart_native/dart_native.dart';
+
+class AVAudioFramePosition {
+  static AVAudioFramePosition fromPointer(Pointer<Void> result) {}
+}
+
+class AVAudioCommonFormat {
+  static AVAudioCommonFormat fromPointer(Pointer<Void> result) {}
+}
+
+class AVAudioFrameCount {}
+
+class NSURL extends NSObject {
+  NSURL.fromPointer(Pointer<Void> result) {}
+}
+
+class NSSecureCoding {}
+
+class AVAudioChannelCount {
+  static AVAudioChannelCount fromPointer(Pointer<Void> result) {}
+}
+
+class AudioStreamBasicDescription {
+  static AudioStreamBasicDescription fromPointer(Pointer<Void> result) {}
+}
+
+class NSData {
+  static NSData fromPointer(Pointer<Void> result) {}
+}
+
+class CMAudioFormatDescriptionRef {
+  static CMAudioFormatDescriptionRef fromPointer(Pointer<Void> result) {}
+}
+
+class AUAudioUnit {
+  static AUAudioUnit fromPointer(Pointer<Void> result) {}
+}
+
+class NSTimeInterval {
+  static NSTimeInterval fromPointer(Pointer<Void> result) {}
+}
+
+class AVAudioNodeBus {}
+
+class NSCopying {}
